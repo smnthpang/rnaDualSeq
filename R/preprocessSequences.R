6ada0ff266e6346323232828a1a4ff43bc5ff0ab
Samantha Pang <114912677+smnthpang@users.noreply.github.com>
2022-11-15 20:40:09 -0500

    Delete preprocessSequences.R

--- R/preprocessSequences.R
+++
@@ -1,9 +0,0 @@
-#' Take two RNA sequences and preprocess them to be able to run DEG analyses on them
-#'
-#'1) Trim sequences
-#'2) Generate a count matrix
-#'
-
-preprocessSequences <- function(){
-
-}
