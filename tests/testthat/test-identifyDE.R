8408ce77acc903d09bcabdb8f84e49a0a4b90b22
samanthapang <samantha.pang@mail.utoronto.ca>
2022-11-15 20:27:43 -0500

    test for identifyED

--- tests/testthat/test-identifyDE.R
+++ tests/testthat/test-identifyDE.R
@@ -2,10 +2,16 @@ library(rnaDualSeq)
 
 setwd("./../..")
 
-test_that("Data files valid", {
+test_that("column names are valid", {
   host <- read_csv("data/Host.data.csv")
   phen <- read_csv("data/Pheno.csv")
   test_norm <- norm_TMM(host, phen)
   test_DE <- identifyDE(test_norm, phen)
-  expect_that(is.matrix(test_norm), equals(TRUE) )
+  #colnames(test_DE[[1]])[1]
+  expect_that(colnames(test_DE[[1]])[1], equals("logFC") )
+  expect_that(colnames(test_DE[[1]])[2], equals("AveExpr") )
+  expect_that(colnames(test_DE[[1]])[3], equals("t"  ) )
+  expect_that(colnames(test_DE[[1]])[4], equals( "P.Value" ) )
+  expect_that(colnames(test_DE[[1]])[5], equals("adj.P.Val") )
+  expect_that(colnames(test_DE[[1]])[6], equals("B"      ) )
 })
